9fda7d76f0966b1f9d215c76095a364b6834a436
jamie <jamiek0414@gmail.com>
2023-09-29 21:04:03 -0400

    final commit???

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,31 @@ public class Main
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean playAgain = true;
+
         //here it is
         while (playAgain) {
             System.out.print("Player A, enter your move [R P S]: ");
-            String playerA = scanner.nextLine();
-            playerA = playerA.toUpperCase();
+            String playerA = scanner.nextLine(); //asks the user the question
+            playerA = playerA.toUpperCase(); // puts in the right form
 
             while (!isValidMove(playerA)) {
-                System.out.print("Invalid move. Please enter [R P S]: ");
+                System.out.print("Invalid move. Please enter [R P S]: "); // forces to do r p s
                 playerA = scanner.nextLine();
                 playerA = playerA.toUpperCase();
             }
 
-            System.out.print("Player B, enter your move [R P S]: ");
+            System.out.print("Player B, enter your move [R P S]: "); //repeat
             String playerB = scanner.nextLine();
             playerB = playerB.toUpperCase();
 
             while (!isValidMove(playerB)) {
-                System.out.print("Invalid move. Please enter [R P S]: ");
+                System.out.print("Invalid move. Please enter [R P S]: "); //same thing
                 playerB = scanner.nextLine();
                 playerB = playerB.toUpperCase();
             }
 
             // Determine the winner
-            String result = determineWinner(playerA, playerB);
+            String result = determineWinner(playerA, playerB); // shows the winner of rps
 
             // Display result
             System.out.println(result);
@@ -44,7 +45,7 @@ public class Main
 
     private static boolean isValidMove(String move) {
         return move.equals("R") || move.equals("P") || move.equals("S");
-    }
+    } // actual code to know who wins
 
     private static String determineWinner(String moveA, String moveB) {
         if (moveA.equals(moveB)) {
